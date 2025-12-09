6ad03ad725fd765c5ab78879cd2ad53ab942a341
Dhruv Singh Jamwal <142426205+dhruvjamwal@users.noreply.github.com>
2025-12-09 16:32:16 +0530

    Update FraudService.java

--- src/main/java/org/example/service/FraudService.java
+++ src/main/java/org/example/service/FraudService.java
@@ -14,7 +14,6 @@ public class FraudService {
     @Autowired
     private TransactionRepository repository;
 
-    // A hardcoded "Blacklist" for demo purposes
     private static final List<String> BLACKLISTED_MERCHANTS = List.of("DarkWeb", "UnknownSite", "HackerStore");
 
     public Transaction processTransaction(Transaction txn) {
@@ -22,21 +21,18 @@ public class FraudService {
         txn.setFraud(false); // Default to safe
         txn.setFraudReason("Safe");
 
-        // --- RULE 1: High Amount Check ---
         if (txn.getAmount() > 50000) {
             txn.setFraud(true);
             txn.setFraudReason("High Value Transaction Alert");
         }
 
-        // --- RULE 2: Blacklisted Merchant Check ---
-        // If the merchant name is in our blacklist, flag it.
+
         else if (BLACKLISTED_MERCHANTS.contains(txn.getMerchant())) {
             txn.setFraud(true);
             txn.setFraudReason("Suspicious Merchant (" + txn.getMerchant() + ")");
         }
 
-        // --- RULE 3: Velocity Check (High Frequency) ---
-        // Check how many transactions this user made in the last 5 minutes
+
         else {
             LocalDateTime fiveMinutesAgo = LocalDateTime.now().minusMinutes(5);
             int recentCount = repository.countByAccountIdAndTimestampAfter(txn.getAccountId(), fiveMinutesAgo);
@@ -47,7 +43,6 @@ public class FraudService {
             }
         }
 
-        // Save result to Database
         return repository.save(txn);
     }
 
