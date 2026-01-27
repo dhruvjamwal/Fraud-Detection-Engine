6b1113ad0fc546ec6d8a86880c5ba4a07cf9d0d8
Dhruv Singh Jamwal <142426205+dhruvjamwal@users.noreply.github.com>
2026-01-27 20:59:20 +0530

    Update EmailService.java

--- Milestone-4/src/main/java/org/example/service/EmailService.java
+++ Milestone-4/src/main/java/org/example/service/EmailService.java
@@ -1,5 +1,4 @@
 package org.example.service;
-
 import org.example.model.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
@@ -11,15 +10,13 @@ public class EmailService {
 
     @Autowired
     private JavaMailSender mailSender;
-
     public void sendFraudAlert(Transaction t) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("prismaticgaming1.0@gmail.com");
-            message.setTo("prismaticgaming1.0@gmail.com");
+            message.setFrom("********@gmail.com");
+            message.setTo("*******@gmail.com");
             message.setSubject("FRAUD DETECTED: Account " + t.getAccountId());
             message.setText("Security Alert:\n\nTransaction #" + t.getId() + " was blocked.\nAmount: " + t.getAmount() + "\nReason: " + t.getFraudReason());
-
             mailSender.send(message);
             System.out.println("EMAIL SENT SUCCESSFULLY");
         } catch (Exception e) {
